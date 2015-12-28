8761608cbf1755454a2065096735431823aa1c4e
Matt Carter <m@ttcarter.com>
2015-12-28 11:18:31 +1000

    BUGFIX: Verbosity fixes

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ module.exports = {
 			.then(function(next) {
 				// Sanity checks {{{
 				if (!mindstate.config.stats.enabled) {
-					if (mindstate.program.verbose) console.log(colors.grey('Stats backup is disabled'));
+					if (mindstate.verbose) console.log(colors.blue('[Stats]'), 'Stats backup is disabled');
 					return next('SKIP');
 				}
 				if (!_.keys(mindstate.config.stats.commands).length) {
-					if (mindstate.program.verbose) console.log(colors.grey('Stats backup has no commands to run'));
+					if (mindstate.verbose) console.log(colors.blue('[Stats]'), 'Stats backup has no commands to run');
 					return next('SKIP');
 				}
 				next();
@@ -32,14 +32,14 @@ module.exports = {
 					.then('binPath', function(next) {
 						which(bin, function(err, path) {
 							if (err) {
-								if (mindstate.program.verbose > 1) console.log(colors.grey('Stats binary command `' + bin + '` is not in PATH'));
+								if (mindstate.verbose > 1) console.log(colors.blue('[Stats]'), 'Stats binary command `' + bin + '` is not in PATH');
 								return next('NOBIN');
 							}
 							next(null, path);
 						});
 					})
 					.then(function(next) {
-						if (mindstate.program.verbose) console.log(colors.blue('[Stats]'), 'Run', command);
+						if (mindstate.verbose) console.log(colors.blue('[Stats]'), 'Run', command);
 						outStream = fs.createWriteStream(workspace.dir + '/' + id + '.txt');
 						next();
 					})
